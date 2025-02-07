45b15a70cc8dfe00c2ff01563a990c1810fe608a
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-02-07 21:44:46 +0000

    Repository Update

---
+++ Week02/Lab/circlearea.cpp
@@ -0,0 +1,26 @@
+/*************** COPY BELOW ***************/
+// This program will output the circumference and area
+// of the circle with a given radius.
+
+#include <iostream>
+using namespace std;
+
+const double PI = 3.14;
+const double RADIUS = 5.4;
+
+int main()
+{
+	//area 								// definition of area of circle 
+	double circumference = 2 * PI * RADIUS;	// computes circumference
+    //double circumference,				// definition of circumference 
+	double area = PI * RADIUS * RADIUS;		// computes area
+    cout << "The circumference of this circle is "<< circumference << "and the radius is" RADIUS
+
+	// Fill in the code for the cout statement that will output (with description)
+	// the circumference
+
+	// Fill in the code for the cout statement that will output (with description)
+	// the area of the circle
+
+	return 0;
+}
---
+++ Week02/Lab/mygrade.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int NumGrade = 86;
+char LetterGrade = 'B';
+
+cout << "The grade which I think I will get is "<< NumGrade << " Which is the letter Grade "<< LetterGrade << endl;
+return 0 ;
+}
