d58604bddabd5ba1e2517dcba500049ead91bdc5
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-02-11 19:16:20 +0000

    Repository Update

--- Week02/HW/checkpoint2_1.cpp
+++ Week02/HW/checkpoint2_1.cpp
@@ -1,13 +1,18 @@
 /* The following C++ program will not compile because the lines have been mixed up. */
-int main()
-}
-// A crazy mixed up program
-return 0;
 
 #include <iostream>
-cout << "In 1492 Columbus sailed the ocean blue.";
-{
 using namespace std;
+
+int main()
+{
+// A crazy mixed up program
+string Stars= "*********************************************************************************************";
+
+
+cout << Stars << endl << "In 1492 Columbus sailed the ocean blue." << endl << Stars << endl;
+return 0;
+}
+
 /*
 When the lines are properly arranged, the program should display the following
 on the screen:
