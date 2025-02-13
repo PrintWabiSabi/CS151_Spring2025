5e55d057c8b6e532d2645d78ef6bef2c857f4144
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-02-13 00:06:23 +0000

    Repository Update

--- Week02/HW/checkpoint2_4.cpp
+++ Week02/HW/checkpoint2_4.cpp
@@ -4,3 +4,12 @@ on the second line, your city, state, and ZIP code on the third line, and your
 telephone number on the fourth line. Place a comment with today’s date at the top
 of the program. Test your program by compiling and running it.
 */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Todays Date
+    cout<< "Shaquille D anger Charlery "<< endl << "Good lands " << endl << "Saint Lucia"<< endl << "758" << endl << "4918300"<< endl;
+return 0;
+}
