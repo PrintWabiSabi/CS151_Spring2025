5fdc679d0c93f73e451a0626f4236dd99e8a7a01
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-02-14 21:26:06 +0000

    Repository Update - 2025-02-14 21:26:06

---
+++ Week03/holiday.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+ string month = "02";
+ string day = "14";
+ string  year = "2025";
+ char delimitor = '/';
+
+ cout <<"Today is "<< month << delimitor << day << delimitor <<  year << " Valentines day. Happy valentines day"<< endl;
+ return 0;
+}
