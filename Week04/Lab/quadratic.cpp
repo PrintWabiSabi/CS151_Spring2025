114a2161efa82baef6d57581e8a9bab3d8c827f6
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-02-21 22:27:59 +0000

    Repository Update - 2025-02-21 22:27:59

--- Week04/Lab/quadratic.cpp
+++ Week04/Lab/quadratic.cpp
@@ -14,12 +14,14 @@ int c;
 int x_1;
 int x_2;
 
-    cout<< "Please input A B C "
+    cout<< "Please input A B C " << endl;
     cin >> a >> b >> c;
 
-    x_1= (-b - sqrt(pow(b,2) - 4*a*c)) / 2*a
+    x_1= (-b + sqrt(pow(b,2) - 4*a*c)) / 2*a;
 
-    cout << "X-1 is = "<<
+    x_2= (-b - sqrt(pow(b,2) - 4*a*c)) / 2*a;
+
+    cout << "X-1 is = "<< x_1 << " and x_2 = " << x_2 <<endl;
 
     return 0;
 
