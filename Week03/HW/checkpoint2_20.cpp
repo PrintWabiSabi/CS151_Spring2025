7a403484bfb2364212d33f71f394f97bbe8fe99c
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-02-21 01:01:07 +0000

    Repository Update - 2025-02-21 01:01:07

--- Week03/HW/checkpoint2_20.cpp
+++ Week03/HW/checkpoint2_20.cpp
@@ -10,3 +10,17 @@ My age is 26 and my weight is 180 pounds.
 (Feel free to lie to the computer about your age and your weight—it’ll
 never know!)
 */
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int age = 31;
+    float weight = 165;
+    
+    cout << "my age is " << age << " and my weight is "<< weight <<" pounds" << endl ;
+    
+    return 0;
+}
