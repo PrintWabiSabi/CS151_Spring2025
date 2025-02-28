4aae9491aea81d45e43d2a52c5f301839de27bcc
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-02-28 21:43:47 +0000

    Repository Update - 2025-02-28 21:43:47

--- Week05/Lab/receipt.cpp
+++ Week05/Lab/receipt.cpp
@@ -23,3 +23,49 @@ Sales Tax    8.25%
 Total        $XX.XX
 
 */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+/*Items        Prices*/
+ double item1  = 1.00;
+ double item2  = 2.00;
+ double item3  = 3.00;
+ double item4  = 4.00;
+ double item5  = 5.00;
+ double item6  = 6.00;
+ double item7  = 7.00;
+ double item8  = 8.00;
+ double item9  = 9.00;
+ double item10 = 10.00;
+ double subtotal = item1 + item2 + item3 + item4 + item5 + item6 + item7 + item8 + item9 + item10;
+ double Sales_tax = 8.25;
+ double total = subtotal * 1.0825;
+
+
+ string name1  = "food";
+ string name2  = "sleep";
+ string name3  = "merch";
+ string name4  = "weather";
+ string name5  = "leroy_jenkins";
+ string name6  = "invader_zim";
+ string name7  = "Todo";
+ string name8  = "Sakura";
+ string name9  = "Tenchi";
+ string name10 = "Washu";
+
+
+
+
+
+
+ cout << setprecision(2) << fixed << total<< endl;
+
+ return 0;
+}
