f3aa7f2ab21c3fc76fc874ccb5a511bffe40a493
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-03-14 18:54:28 +0000

    Repository Update - 2025-03-14 18:54:27

--- Week07/week07.cpp
+++ Week07/week07.cpp
@@ -7,6 +7,39 @@
 
 using namespace std;
 
+void quiz01_ec()
+{
+    cout << "Programing is fun! \n";
+}
+void quiz02_ec()
+{
+    long byte = 8;
+    long gigabyte = pow(2,30) * byte ;
+    long answer = 2 * gigabyte; 
+    cout << answer << endl;
+    cout << pow(2,34)<< endl;
+}
+
 int main() {
+
+    cout <<  "Please enter the quiz number (106) for the extra";
+    cout << "credit you would like to see: "; 
+    cin >> choice; 
+
+    switch (choice)
+    {
+       case 1 : cout << "Choice 1\n"';
+                 break;
+       case 1 : cout << "Choice 1\n"';
+                 break;
+       case 1 : cout << "Choice 1\n"';
+                 break;           
+       case 1 : cout << "Choice 1\n"';
+                 break;
+       case 1 : cout << "Choice 1\n"';
+                 break;
+       case 1 : cout << "Choice 1\n"';
+                 break;                    
+    }
     
 }
