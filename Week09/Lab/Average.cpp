1d5b042cb26b0eb5fff29e08619bfb80fe9d9b65
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-03-28 22:05:46 +0000

    Repository Update - 2025-03-28 22:05:46

--- Week09/Lab/Average.cpp
+++ Week09/Lab/Average.cpp
@@ -3,3 +3,26 @@
 #include<cstdlib>
 
 using namespace std;
+
+int main() 
+{
+srand(time(0));
+
+const int N = rand()%16+5; // This gives us a random number from 5-20
+
+cout<< N << endl;  // This prints out N which right now is a random number from 5-20
+
+int nums[ N ];  // This is how you declare an array of size N and size N will be the random number generate on line  11
+
+cout <<"[ ";
+for(int count  = 0 ; count < N ; count++){
+    nums[count] = rand()%101;
+    if( count < N-1 )
+    { 
+        cout<< nums[count] <<',';
+        } else{ 
+            cout<<nums[count];
+        }
+
+ } cout<<']' <<endl; 
+} // N-- = N-1 and also N-=1 and N=N-1
