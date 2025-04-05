13a6cbc08fd3a8b9ff5f7927b1114ce91b465922
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-04-05 21:53:33 +0000

    Repository Update - 2025-04-05 21:53:32

--- Week09/HW/checkpoint5_03.cpp
+++ Week09/HW/checkpoint5_03.cpp
@@ -2,3 +2,24 @@
 Write an input validation loop that asks the user to enter ‘Y’, ‘y’, ‘N’, or ‘n’.
 */
 
+#include<iostream>
+
+
+
+using namespace std; 
+
+int main(){
+
+    char input;
+    
+    cout << " Please enter ‘Y’, ‘y’, ‘N’, or ‘n’ " << endl;
+    cin >> input;
+    
+    while (input !='Y' && input != 'y' && input != 'N' && input!='n' )
+    {  cout<< "invlaid input. Please enter ‘Y’, ‘y’, ‘N’, or ‘n’ " << endl;
+      cin>> input; 
+    }
+   cout << "your input is valid " << input << endl;
+   
+      return 0;
+    }
