c5eae34bdd19279b5ad2972532ebcae612a50c31
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-04-05 22:11:46 +0000

    Repository Update - 2025-04-05 22:11:46

--- Week09/HW/checkpoint5_04.cpp
+++ Week09/HW/checkpoint5_04.cpp
@@ -2,3 +2,22 @@
 Write an input validation loop that asks the user to enter “Yes” or “No”.
 */
 
+#include<iostream>
+
+
+using namespace std;
+
+int main() 
+{
+string input;
+
+cout << "Please enter Yes or No " << endl;
+cin >> input;
+    
+ while (input != "Yes" && input != "No") {
+ cout << " Answer is not valid Please enter Yes or No << endl; 
+ cin >> input;
+    }
+  cout<< "Your answer was valid and your answer is " << input <<endl;
+  
+return 0; }
