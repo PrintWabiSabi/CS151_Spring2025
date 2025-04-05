420f2b8fe551916140aaab4bb7898f0f77e9f101
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-04-05 21:26:54 +0000

    Repository Update - 2025-04-05 21:26:54

--- Week09/HW/checkpoint5_02.cpp
+++ Week09/HW/checkpoint5_02.cpp
@@ -3,3 +3,27 @@ Write an input validation loop that asks the user to enter a number in the range
 of 10 through 25.
 */
 
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+
+    int input;
+
+    cout<< "Please enter a number in range of 10 through 25" << endl;
+    cin >> input;
+
+    while ( input < 10 ||  input > 25 ) {
+        cout<< "your input is invalid. please enter a number in range 10-25 " << input << endl;
+        cin >> input;}
+
+        cout << "your input is valid " << input << endl;
+    
+
+    
+
+return 0;
+}
