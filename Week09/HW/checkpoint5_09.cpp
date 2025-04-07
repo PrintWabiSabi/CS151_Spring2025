ebdeb7654dedee7ec8aac3d35ec5bc86295bc039
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-04-07 01:59:25 +0000

    Repository Update - 2025-04-07 01:59:25

--- Week09/HW/checkpoint5_09.cpp
+++ Week09/HW/checkpoint5_09.cpp
@@ -1,3 +1,16 @@
 /*
 Write a for loop that displays your name 10 times.
 */
+
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int x;
+    for( x = 1; x < 11; ++x)
+    cout <<" your name" << endl;
+}
+
