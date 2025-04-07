8962282cd78a0ddd7142eb89405977605d1ff02b
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-04-07 02:24:49 +0000

    Repository Update - 2025-04-07 02:24:49

--- Week09/HW/checkpoint5_11.cpp
+++ Week09/HW/checkpoint5_11.cpp
@@ -1,3 +1,16 @@
 /*
 Write a for loop that displays every fifth number, 0 through 100.
 */
+
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+    cout << " number will go from 0 - 100 " << endl;
+    int x;
+    for( x = 0; x <= 100; x = x +5)
+    cout <<"The number now is " <<  x << endl;
+}
