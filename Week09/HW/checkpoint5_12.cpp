a718a519d183524f7b628cc94126677d9c3fa360
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-04-07 02:46:11 +0000

    Repository Update - 2025-04-07 02:46:10

--- Week09/HW/checkpoint5_12.cpp
+++ Week09/HW/checkpoint5_12.cpp
@@ -2,3 +2,24 @@
 Write a for loop that repeats seven times, asking the user to enter a number. The
 loop should also calculate the sum of the numbers entered.
 */
+
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+   int sum = 0;
+   int number;
+
+   cout <<"Please enter seven number"  << endl;
+
+    for( int x = 1 ; x <= 7; x++)
+    {  
+    cin >>  number;
+    sum += number;      }
+    cout << "The sum of the numbers is " << sum << endl;
+
+    return 0;
+}
