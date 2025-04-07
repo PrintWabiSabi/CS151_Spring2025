c6329ad3f966d018b917b232c0ec534ea485543b
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-04-07 02:12:17 +0000

    Repository Update - 2025-04-07 02:12:17

--- Week09/HW/checkpoint5_10.cpp
+++ Week09/HW/checkpoint5_10.cpp
@@ -1,3 +1,14 @@
 /*
 Write a for loop that displays all of the odd numbers, 1 through 49.
 */
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int x;
+    for( x = 1; x < 50; x = x + 2 )
+    cout << x << endl;
+}
