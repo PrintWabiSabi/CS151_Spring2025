daca54f2416c65a7a82b070caacf76f1a52614a3
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-04-11 19:36:14 +0000

    Repository Update - 2025-04-11 19:36:14

---
+++ Week11/Lab/Void.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Sayhello();
+
+int main(){
+
+    void Sayhello(string name)
+    {
+        cout << "Hello " << name << endl;
+    }
+
+
+}
