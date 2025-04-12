bbab33a087c9aeee8c85a3a2508968c4b029c457
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-04-12 02:34:26 +0000

    Repository Update - 2025-04-12 02:34:26

--- Week10/HW/checkpoint5_22.cpp
+++ Week10/HW/checkpoint5_22.cpp
@@ -2,3 +2,12 @@
 5.22 - Write a short program that uses a for loop to write the numbers 1 through 10 to
        a file.
 */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+       for (int i = 1; i <= 10; i++)
+       cout<< i << endl;
+    return 0;
+}
--- Week11/Lab/Void.cpp
+++ Week11/Lab/Void.cpp
@@ -5,14 +5,44 @@
 
 using namespace std;
 
-void Sayhello();
+void Sayhello(string name)
+    {
+         if ( name == ""){
+            name = "World";
+        }
+        cout << "Hello " << name << endl;
+    }
+    //for( int x = 1 ; x <= 7; x++)
+  /*  {  
+        cin >>  number;
+        sum += number;      }
+        cout << "The sum of the numbers is " << sum << endl;
+*/    
+
+
+void countdown(int n){
+    if( n < 0){
+        cout << "invalid Input" << endl;
+        return;
+    }
+    for(int x = n; x >= 0; x--){
+        cout << x << endl;
+    }
+}
+
+void printBlock(int n){
+    for(int x = 0; x < n; x++){
+        for(int x = 0; x < n; x++)
+        cout<< '*';
+    
+    }
+
+}
+
 
 int main(){
 
-    void Sayhello(string name)
-    {
-        cout << "Hello " << name << endl;
-    }
-
-
+    Sayhello("");
+    countdown(10);
+    printBlock(10);
 }
