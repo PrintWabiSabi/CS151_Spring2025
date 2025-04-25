9030743a3e82732a60e91fc2c837271368de3165
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-04-25 19:03:19 +0000

    Repository Update - 2025-04-25 19:03:19

---
+++ Week13/Lab/Funcs.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include<cstdlib>
+
+using namespace std;
+
+void sayHello(string A =  "World!"){ 
+    cout << "Hello "<< A << endl;
+}
+
+
+int main(){
+      
+    sayHello("Eve");
+    return 0;
+}
+
+
+
+
+
+
+
