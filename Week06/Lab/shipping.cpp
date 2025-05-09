447272813cb9fb53f0d74ec07b84433dbcd4af85
PrintWabiSabi <116389154+PrintWabiSabi@users.noreply.github.com>
2025-05-09 21:06:03 +0000

    Repository Update - 2025-05-09 21:06:03

--- Week06/Lab/shipping.cpp
+++ Week06/Lab/shipping.cpp
@@ -5,22 +5,45 @@
 using namespace std;
 int main ()
 {
-    int weight;
+    double weight;
+    double  miles;
+    double shipping = 0;
+    double rate = 0;
 
     cout <<  "Please input the package weight in Kg " << endl;
     cin >> weight;
+    cout << "Plesase input distance in miles ";
+    cin >> miles;
+    shipping =  miles;
+    
+    if (weight >= 0 && weight <= 20 && miles >= 10 &&  miles <= 3000 ){
 
-    if ( weight <= 2 )
-    {cout << " The cost of shipping is 1.50" << endl; }
+        if ( weight <= 2 && weight >= 0 )
+    {  rate = 1.10;
+        shipping = rate * (miles / 500);
 
-    else if (weight <= 3 )
-    {cout << " The cost of shipping is 2.20" << endl; }
+        cout << " The cost of shipping is " << shipping << endl; }
 
-    else if (weight <= 6)
-    {cout << " The cost of shipping is 3.70 " << endl; }
+    else if (weight > 2 && weight <=6 ){
+        rate = 2.20;
+        shipping = rate * (miles / 500);
+        cout << " The cost of shipping is "<< shipping << endl; }
 
-    else (weight <= 10 && weight >= 20);
-    {cout << "The cost of shipping is 4.80 "<< endl; };
+    else if (weight > 6 && weight <= 10 )
+    {   
+         rate = 3.70;
+        shipping = rate * (miles / 500);
+        cout << " The cost of shipping is "<< shipping << endl; }
+
+    else if (weight > 10 && weight <= 20)
+    {    rate = 4.80;
+        shipping = rate * (miles / 500);
+        cout << "The cost of shipping is " << shipping << endl; 
+    }
+    }
+     else   {
+        cout << "The weight and or milage is invalid ";
+     }
 
     return 0;
 }
